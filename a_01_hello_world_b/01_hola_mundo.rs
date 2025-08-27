e041eff509258360d910096a8173fc42d80376cb
SrLLamaDev <srllamadev@gmail.com>
2025-08-27 15:43:31 -0400

    execute mode

---
+++ a_01_hello_world_b/01_hola_mundo.rs
@@ -0,0 +1,5 @@
+// Ejemplo 1: Hola Mundo Básico
+fn main() {
+    println!("¡Hola, mundo!");
+    println!("¡Bienvenido al curso de Rust! 🦀");
+}
---
+++ a_01_hello_world_b/README.md
@@ -0,0 +1,3 @@
+###How to execute
+rustc 01_hola_mundo.rs
+./01_hola_mundo
